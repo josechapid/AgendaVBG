1e42a46bd44bf00638de68c197537e70f83ef64f
gastondandrea <gastondandrea@hotmail.com>
2024-05-10 16:39:46 -0300

    dando escructura

--- AgendaVBG/App.js
+++ AgendaVBG/App.js
@@ -1,20 +1,22 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import PerfileScreen from './component/PerfileScreen/PerfileScreen'
+import HomeScreen from './component/HomeScreen/HomeScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Agenda funcionando !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} initialParams={{fromChild: 'Initial'}} />
+        <Stack.Screen name="Profile" component={PerfileScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
