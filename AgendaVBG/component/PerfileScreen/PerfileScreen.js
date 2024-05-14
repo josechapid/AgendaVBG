6250e54db7d619fce0f5e25d2b339b7cacddd28c
luischapid <luischapid03@gmail.com>
2024-05-14 11:38:45 -0500

    todo listo

--- AgendaVBG/component/PerfileScreen/PerfileScreen.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-const PerfileScreen = ({route}) => {
-    return <Text> This is {route.params.name}'s profile </Text>;
-};
-
-
-export default PerfileScreen;
