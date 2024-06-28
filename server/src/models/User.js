d68ff32d80d367a20eef04d6d77198c7edb8c417
luischapid <luischapid03@gmail.com>
2024-06-28 16:46:34 -0500

    creando modelos y relaciones

---
+++ server/src/models/User.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      dateOfBirth: {
+        type:DataTypes.DATE,
+        allowNull: true
+      },
+      address: {
+        type:DataTypes.STRING,
+        allowNull: true
+      },
+      phone: {
+        type:DataTypes.INTEGER,
+        allowNull: true
+      }
+    },
+    { timestamps: false }
+  );
+};
