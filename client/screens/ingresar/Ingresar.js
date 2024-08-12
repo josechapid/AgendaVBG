dc2332d4d05a313e6f7174fff12562492f2ce31e
luischapid <luischapid03@gmail.com>
2024-08-12 08:23:45 -0500

    cambios para parrafos de ingresar

--- client/screens/ingresar/Ingresar.js
+++ client/screens/ingresar/Ingresar.js
@@ -8,18 +8,28 @@ import { useNavigation } from '@react-navigation/native';
 const { width: viewportWidth } = Dimensions.get('window'); //para que cubra el ancho del dispositivo
 
 const Ingresar = () => {
-    const navigation = useNavigation();
-    const carouselRef = useRef(null); 
-
-    const entries = [ //array para el carrousel
-    { description: "La violencia es el “uso intencional de la fuerza física o el poder real o como amenaza contra uno mismo, una persona, grupo o comunidad que tiene como resultado la probabilidad de daño psicológico, lesiones, la muerte, privación o mal desarrollo.", 
-    title: 'Slide 1', image: require('../../assets/img/carousel/Slice_1.jpg') },
-    {description: "La violencia es el “uso intencional de la fuerza física o el poder real o como amenaza contra uno mismo, una persona, grupo o comunidad que tiene como resultado la probabilidad de daño psicológico, lesiones, la muerte, privación o mal desarrollo.", 
-    title: 'Slide 2', image: require('../../assets/img/carousel/Slice_2.jpg')  },
-    {description: "La violencia es el “uso intencional de la fuerza física o el poder real o como amenaza contra uno mismo, una persona, grupo o comunidad que tiene como resultado la probabilidad de daño psicológico, lesiones, la muerte, privación o mal desarrollo.", 
-    title: 'Slide 3', image: require('../../assets/img/carousel/Slice_3.jpg')  },
+  const navigation = useNavigation();
+  const carouselRef = useRef(null);
+  const entries = [
+    {
+      description:
+        "La violencia contra la mujer -especialmente la ejercida por su pareja y la violencia sexual- constituye un grave problema de salud pública y una violación de los derechos humanos de las mujeres. La violencia puede afectar negativamente la salud física, mental, sexual y reproductiva de las mujeres.",
+      title: "Datos Clave",
+      image: require("../../assets/img/carousel/Slice_1.jpg"),
+    },
+    {
+      description:
+        "Las estimaciones publicadas por la OMS indican que alrededor de una de cada tres mujeres en las Américas han sufrido violencia física y/o sexual de pareja o violencia sexual por terceros en algún momento de su vida.",
+      title: "Datos Clave",
+      image: require("../../assets/img/carousel/Slice_2.jpg"),
+    },
+    {
+      description:
+        "El sistema de salud puede desempeñar un papel vital en responder y prevenir la violencia contra las mujeres y las niñas. Este papel incluye identificar el abuso temprano, proporcionar atención y apoyo a las sobrevivientes, y referir a las mujeres a servicios adecuados e informados dentro y fuera del sistema sanitario",
+      title: "La violencia se puede prevenir",
+      image: require("../../assets/img/carousel/Slice_3.jpg"),
+    },
   ];
-
 
   return (
     <View style={styles.container}>
@@ -30,35 +40,37 @@ const Ingresar = () => {
         height={600}
         autoPlay={true}
         data={entries}
-        scrollAnimationDuration={1000}
+        scrollAnimationDuration={3000}
         renderItem={({ item }) => (
           <View style={styles.slide}>
             <Image source={item.image} style={styles.image} />
             <Text style={styles.title}>{item.title}</Text>
             <ScrollView style={styles.descriptionContainer}>
-                <Text style={styles.text} >{item.description}</Text>
+              <Text style={styles.text}>{item.description}</Text>
             </ScrollView>
           </View>
         )}
       />
       <View style={styles.sectionLogin}>
         <View style={styles.buttonSec}>
-          <TouchableOpacity 
+          <TouchableOpacity
             style={styles.button}
-            onPress={() => navigation.navigate("SingIn")}>
+            onPress={() => navigation.navigate("SingIn")}
+          >
             <Text style={styles.buttonText}>Ingresar</Text>
           </TouchableOpacity>
         </View>
-          <View style={styles.buttonSec}>
-            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-              <Text style={styles.loginText}>No tienes cuenta? Registrate aqui</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.buttonSec}>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+            <Text style={styles.loginText}>
+              No tienes cuenta? Registrate aqui
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
-    
   );
-}
+};
 
 
 
--- client/screens/login/Login.js
+++ client/screens/login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useDispatch } from "react-redux";
@@ -6,114 +6,113 @@ import { setNewUser } from "../../redux_toolkit/features/counter/Slice";
 import axios from "axios";
 import styles from "./styles";
 
-function Login () {
-    const navigation= useNavigation();
-    const dispatch=useDispatch();
-    const [name, setName] = useState("");
-    const [user, setUser] = useState("");
-    const [dateOfBirth, setDateOfBirth] = useState("");
-    const [address, setAddress] = useState("");
-    const [phone, setPhone] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+function Login() {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [user, setUser] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    async function sendDates(){
-      if(password !== confirmPassword){
-        Alert.alert("Las contraseñas no coinciden")
-      }
-      try {
-        const dates = {
-           name,
-           user,
-           dateOfBirth,
-           address,
-           phone,
-           email,
-          password,
-        };
-        const response = await axios.post(
-          "https://agendavbg.onrender.com/user",
-          dates
-        );
-        if(response.data){
-          dispatch(setNewUser(response.data));
-          navigation.navigate("Main");
-        }
-      } catch (error) {
-        console.error("No se pueden enviar los datos: ", error)
-      }
+  async function sendDates() {
+    if (password !== confirmPassword) {
+      Alert.alert("Las contraseñas no coinciden");
     }
+    try {
+      const dates = {
+        name,
+        user,
+        dateOfBirth,
+        address,
+        phone,
+        email,
+        password,
+      };
+      const response = await axios.post(
+        "https://agendavbg.onrender.com/user",
+        dates
+      );
+      if (response.data) {
+        dispatch(setNewUser(response.data));
+        navigation.navigate("Main");
+      }
+    } catch (error) {
+      console.error("No se pueden enviar los datos: ", error);
+    }
+  }
 
-    return (
-      <View style={styles.login}>
-        {/* ------------------------------------section logo y title */}
-        <View style={styles.headerContainer}>
-          <Text style={styles.title}>Crea tu cuenta en </Text>
-          <Text style={styles.titleInitial}>Renacer Juntas</Text>
-        </View>
-        <View style={styles.formContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Nombre completo"
-            value={name}
-            onChangeText={setName}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Usuario"
-            value={user}
-            onChangeText={setUser}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Fecha de Nacimiento ej. 26 october 1993 "
-            value={dateOfBirth}
-            onChangeText={setDateOfBirth}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Dirección"
-            value={address}
-            onChangeText={setAddress}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Teléfono"
-            value={phone}
-            onChangeText={setPhone}
-            /* keyboardType="phone-pad" */
-            inputMode="numeric"
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Correo Electrónico"
-            value={email}
-            onChangeText={setEmail}
-            /* keyboardType="email-address" */
-            inputMode="email"
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Contraseña"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Repetir Contraseña"
-            value={confirmPassword}
-            onChangeText={setConfirmPassword}
-            secureTextEntry
-          />
-          <TouchableOpacity style={styles.button} onPress={sendDates}>
-            <Text style={styles.buttonText}>Enviar</Text>
-          </TouchableOpacity>
-        </View>
+  return (
+    <View style={styles.login}>
+      {/* ------------------------------------section logo y title */}
+      <View style={styles.headerContainer}>
+        <Text style={styles.title}>Crea tu cuenta en </Text>
+        <Text style={styles.titleInitial}>Renacer Juntas</Text>
       </View>
-    );
-};
-
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre completo"
+          value={name}
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Usuario"
+          value={user}
+          onChangeText={setUser}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Fecha de Nacimiento ej. 26 october 1993 "
+          value={dateOfBirth}
+          onChangeText={setDateOfBirth}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Dirección"
+          value={address}
+          onChangeText={setAddress}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Teléfono"
+          value={phone}
+          onChangeText={setPhone}
+          /* keyboardType="phone-pad" */
+          inputMode="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Correo Electrónico"
+          value={email}
+          onChangeText={setEmail}
+          /* keyboardType="email-address" */
+          inputMode="email"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Repetir Contraseña"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.button} onPress={sendDates}>
+          <Text style={styles.buttonText}>Enviar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
 
 export default Login;
