763ae079056d1a25dfb5b68342a2f7deda374114
luischapid <luischapid03@gmail.com>
2024-08-28 21:47:11 -0500

    agregando get a howDoIFeel

---
+++ server/src/handlers/howDoIFeel/getHowDoIFeeelHandler.js
@@ -0,0 +1,17 @@
+const getHowDoIFeelController= require ("../../controllers/howDoIFeel/getHowDoIFeeelController")
+
+const getHowDoIFeelHandler= async (req, res)=>{
+try {
+    const {user_id, number}= req.body
+    const response= await getHowDoIFeelController(user_id, number)
+    if(response){
+        res.status(200).json(response)
+    } else{
+        res.status(400).json(response.success)
+    }
+} catch (error) {
+    res.status(500).json({ error: error.message });
+}
+}
+
+module.exports= getHowDoIFeelHandler
